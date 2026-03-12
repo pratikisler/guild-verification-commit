1df341b9bb752c12176409511c99cac8344c3d3c
pratikisler <166455670+pratikisler@users.noreply.github.com>
2026-03-12 16:34:09 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T13:07:13.459Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-12T13:34:10.259Z
+Commit: Fix CSS styling bug on mobile devices
