d3cb41446caa75bef8da379b8c87832d974acd67
pratikisler <166455670+pratikisler@users.noreply.github.com>
2026-03-12 16:34:27 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T13:34:27.137Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-12T13:34:28.358Z
+Commit: Patch API endpoint error handling
