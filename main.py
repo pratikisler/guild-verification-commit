301a4f275facd83fa1f8dfc3c87936529f1f4d37
pratikisler <166455670+pratikisler@users.noreply.github.com>
2026-03-12 16:07:35 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T13:07:27.030Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-12T13:07:36.589Z
+Commit: Fix broken links in documentation
