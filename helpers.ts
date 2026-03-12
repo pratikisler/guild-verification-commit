27e8928075ea8d19710964934e6949999a51eaeb
pratikisler <166455670+pratikisler@users.noreply.github.com>
2026-03-12 16:33:33 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T13:33:28.037Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-12T13:33:35.032Z
+Commit: Patch API endpoint error handling
