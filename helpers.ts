269c9b8192cfdd6d34a069e7ccac6936d730cfad
pratikisler <166455670+pratikisler@users.noreply.github.com>
2026-03-12 16:07:19 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T13:07:03.969Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-12T13:07:20.350Z
+Commit: Fix race condition in async operations
