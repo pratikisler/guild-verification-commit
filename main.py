2c74695ec857141f97cb298f23e0eeeac4abdc1a
pratikisler <166455670+pratikisler@users.noreply.github.com>
2026-03-12 16:40:19 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T13:39:47.626Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-12T13:40:20.378Z
+Commit: Fix broken links in documentation
