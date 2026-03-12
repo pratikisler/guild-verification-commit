ff313e47af5b2b600614df0fb648e38cff3f42f4
pratikisler <166455670+pratikisler@users.noreply.github.com>
2026-03-12 16:39:46 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T13:34:41.155Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-12T13:39:47.626Z
+Commit: Fix memory leak in event handlers
