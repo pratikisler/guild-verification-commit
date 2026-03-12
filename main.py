ebcb365e5e7c20ebd6fd93714a22913e37efd8c8
pratikisler <166455670+pratikisler@users.noreply.github.com>
2026-03-12 16:07:26 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T13:07:07.417Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-12T13:07:27.030Z
+Commit: Introduce real-time notifications
