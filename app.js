187a7870f0213f4021fafb17d2ebaf99e26c81c9
pratikisler <166455670+pratikisler@users.noreply.github.com>
2026-03-12 16:40:22 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T13:40:17.798Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-12T13:40:23.906Z
+Commit: Fix broken links in documentation
